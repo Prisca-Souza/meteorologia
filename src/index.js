804d0b22d5e6f3d0347b918c8b0263d99dbbb184
Prisca Souza <priscasouza11@gmail.com>
2024-09-27 15:38:10 -0300

    The sections where I'm assigning values to the HTML elements (like `cityElement.innerHTML = ...`) are outside the `refreshWeather` function.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  let iconElement = document.querySelector("#icon");
+
   
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
@@ -15,9 +16,8 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElemenet.innerHTML = Math.round(temperature);
-  let iconElement = document.querySelector("#icon");
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
-
 function formatDate(date) {
   
   let minutes = date.getMinutes();
